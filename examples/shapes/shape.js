1426acfe28b42991f30981921c09531d44907d0c
ncardozo <n.cardozo@uniandes.edu.co>
2023-05-30 12:19:17 +0200

    updated shape example
    Signed-off-by: ncardozo <n.cardozo@uniandes.edu.co>

--- examples/shapes/shape.js
+++ examples/shapes/shape.js
@@ -1,8 +1,8 @@
 const cop = require("./context-traits.js");
 
 TShape = {
-	b: 0,
-	h: 0,
+	b: 2,
+	h: 3,
 	type: "shape",
 	getType: function() {
 		return this.type;
@@ -12,6 +12,9 @@ TShape = {
 	},
 	perimeter: function() {
 		return 'Calling an abstract method perimeter';
+	},
+	numberOfSides: function() {
+		return 0;
 	}
 };
 
@@ -25,6 +28,9 @@ TriangleBehavior = cop.Trait({
 	},
 	perimeter: function() {
 		return this.b + 2*Math.sqrt(Math.pow(this.h,2) + Math.pow(this.b/2, 2));
+	},
+	numberOfSides: function() {
+		return 3;
 	}
 });	
 Triangle.adapt(TShape, TriangleBehavior);
@@ -41,10 +47,22 @@ CircleBehavior = cop.Trait({
 	},
 	perimeter: function() {
 		return this.b * 2* Math.PI;
+	},
+	numberOfSides: function() {
+		throw new Error("Number of sides is not defined in a circle");
 	}
 });	
 Circle.adapt(TShape, CircleBehavior);
 
-module.exports.TShape = TShape;
-module.exports.Circle = TShape;
-module.exports.Triangle = TShape;
+if(matchMedia.random() >= 0.5) 
+	Circle.activate()
+else 
+	Triangle.activate();
+
+var area = TShape.area();
+var sides = TShape.numberOfSides();
+
+if(Circle.isActive()) 
+	this.Circle.deactivate();
+else
+	Triangle.deactivate();
